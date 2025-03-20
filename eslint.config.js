4f51ac6583f45dfa2e41acc5031e7cf12c666789
Aleksandr010101 <Alexkotovski16@gmail.com>
2025-03-20 16:41:47 +0300

    modified:   .gitignore 	modified:   eslint.config.js 	modified:   index.js 	modified:   package-lock.json 	modified:   package.json Updated eslint plugins. Set up eslint.config. Add gitignor vscode/settings.json

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,11 @@ import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
 import pluginJs from '@eslint/js';
 
+
+import prettierPlugin from 'eslint-plugin-prettier';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import plugin from 'eslint-plugin-react';
+
 // mimic CommonJS variables -- not needed if using CommonJS
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -14,6 +19,29 @@ const compat = new FlatCompat({
 });
 
 export default [
-  { languageOptions: { globals: globals.node } },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+        ...globals.es2022,
+    },
+  },
   ...compat.extends('airbnb'),
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+  },
+  {
+    ignores: ['node_modules', 'dist'],
+  },
+  {
+    files: ['**/*.{js,jsx}'],
+    rules: {
+      ...eslintConfigPrettier.rules,
+      'prefer-const': 'error',
+    },
+  },
+ }
 ];
--- index.js
+++ index.js
@@ -2,5 +2,5 @@ import _ from 'lodash';
 
 import { myLast } from './src/index.js';
 
-const result = myLast('one', 'two');
+let result = myLast('one', 'two');
 console.log(result);
